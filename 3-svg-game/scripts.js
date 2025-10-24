ae40e2c6da884d66de0cf397e6139407db4ecebc
jMas54 <jmastrangelo@halix.io>
2025-10-24 07:48:03 -0400

    Added credit to images.

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -14,6 +14,10 @@ let TIMER_INTERVAL = 1000;
 const popupOverlay = $("#popup-overlay");
 const closePopupBtn = $("#closePopupBtn");
 
+// Stores each asteroid in a list so that its position can updated and accessed 
+let asteroids = ["asteroid0" ,"asteroid1", "asteroid2", "asteroid3", "asteroid4"];
+
+
 $(function() { //Shorthand that allows a function to be run as soon as page loads
    showInstructions();
 });
@@ -28,43 +32,11 @@ function hideInstructions() {
     animateObjects();
 }
 
-// Allows user input to control the rocketship as soon as the website loads
-document.addEventListener("DOMContentLoaded", () => {
-    const rocketship = document.getElementById("rocketship");
-    let x = parseInt(rocketship.getAttribute('x')); // Get initial x position
-    
-    // How much the rocketship moves to the left/right when the arrow keys are pressed
-    const step = 50; 
-    
-document.addEventListener("keydown", (event) => {
-    switch (event.key) {
-        case "ArrowLeft":
-            if (x > 0){
-                x -= step;
-            }
-        break;
-        case "ArrowRight":
-            if (x < 400){
-                x += step;
-            }
-        break;
-    }
-    // Update the SVG element's x attribute
-    rocketship.setAttribute("x", x);
-    SHIP_CURRENT_X = x; 
-
-    });
-});
-    
-// Stores each asteroid in a list so that its position can updated and accessed 
-let asteroids = ["asteroid0" ,"asteroid1", "asteroid2", "asteroid3", "asteroid4"];
-
 // Starts the motion of the asteroids
 function animateObjects(){
     startTimer();
     ANIMATION_INTERVAL = setInterval(moveObjects, 2);
 }
-
 
 // Updates the postions of the asteroids and resets it to the top of the screen when it passes the bottom 
 // Also checks for collision
@@ -171,3 +143,31 @@ function updateTimer() {
 
     document.getElementById("timerDisplay").textContent = `${displayHours}:${displayMinutes}:${displaySeconds}`;
 }
+
+// Allows user input to control the rocketship as soon as the website loads
+document.addEventListener("DOMContentLoaded", () => {
+    const rocketship = document.getElementById("rocketship");
+    let x = parseInt(rocketship.getAttribute('x')); // Get initial x position
+    
+    // How much the rocketship moves to the left/right when the arrow keys are pressed
+    const step = 50; 
+    
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "ArrowLeft":
+            if (x > 0){
+                x -= step;
+            }
+        break;
+        case "ArrowRight":
+            if (x < 400){
+                x += step;
+            }
+        break;
+    }
+    // Update the SVG element's x attribute
+    rocketship.setAttribute("x", x);
+    SHIP_CURRENT_X = x; 
+
+    });
+});
